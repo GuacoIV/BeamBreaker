bf6104bc00624b964ddc1249ca1eb4592dc351be
GuacoIV <edward.lynch@att.net>
2014-04-05 14:17:26 -0500

    Line drawing

--- src/com/lsu/beambreaker/BeamBreaker.java
+++ src/com/lsu/beambreaker/BeamBreaker.java
@@ -1,7 +1,11 @@
 package com.lsu.beambreaker;
 
+import java.util.Random;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Paint;
 import android.view.Menu;
 import android.widget.RelativeLayout;
@@ -18,8 +22,9 @@ public class BeamBreaker extends Activity
 		setContentView(R.layout.activity_beam_breaker);
 		RelativeLayout parentLayout = (RelativeLayout) findViewById(R.id.ParentLayout);
 		LayoutParams params = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
-		//myCanvas.setLayoutParams(params);
 		parentLayout.addView(myCanvas, 0, params);
+		
+
 	}
 
 	@Override
@@ -29,5 +34,13 @@ public class BeamBreaker extends Activity
 		getMenuInflater().inflate(R.menu.beam_breaker, menu);
 		return true;
 	}
-
+	// navigation bar (at the bottom of the screen)
+	public static int getNavigationBarHeight(Context context) {
+	    Resources resources = context.getResources();
+	    int resourceId = resources.getIdentifier("navigation_bar_height", "dimen", "android");
+	    if (resourceId > 0) {
+	        return resources.getDimensionPixelSize(resourceId);
+	    }
+	    return 0;
+	}
 }
--- src/com/lsu/beambreaker/MyCanvas.java
+++ src/com/lsu/beambreaker/MyCanvas.java
@@ -1,6 +1,9 @@
 package com.lsu.beambreaker;
 
+import java.util.Random;
+
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -9,21 +12,32 @@ import android.view.View;
 
 public class MyCanvas extends View
 {
-
+	Paint paint = new Paint();
+	public static Context baseContext;
+	final int width = getResources().getDisplayMetrics().widthPixels;
+	final int height;
+	
 	public MyCanvas(Context context)
 	{
 		super(context);
+		Random r = new Random();
+		height = getResources().getDisplayMetrics().heightPixels - BeamBreaker.getNavigationBarHeight(context);
 	}
 
 	@Override
 	protected void onDraw(Canvas canvas)
 	{
-		Paint paint = new Paint();
+		
 		paint.setColor(Color.BLUE);
 		paint.setTextSize(50);
 		paint.setTextAlign(Align.CENTER);
-		canvas.drawText("Yo ho ho!", 100, 100, paint);
+		//canvas.drawText("Yo ho ho!", 100, 100, paint);
+		canvas.drawLine(0, 0, width, height, paint);
+		canvas.drawLine(width/3, 0, width/3 + width/5, height, paint);
+		canvas.drawLine(width, 0, 0, height, paint);
 		super.onDraw(canvas);
 	}
+	
+
 
 }
