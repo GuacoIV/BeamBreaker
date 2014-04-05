51440a84ea71d4c7a0994a28b3f0a275cc7f232d
GuacoIV <edward.lynch@att.net>
2014-04-05 16:11:05 -0500

    Lines zap away

--- src/com/lsu/beambreaker/MyCanvas.java
+++ src/com/lsu/beambreaker/MyCanvas.java
@@ -19,11 +19,13 @@ public class MyCanvas extends View
 	final int width = getResources().getDisplayMetrics().widthPixels;
 	final int height;
 	float points[];
+	int linesZapped = 0;
 	public MyCanvas(Context context)
 	{
 		super(context);
 		Random r = new Random();
 		height = getResources().getDisplayMetrics().heightPixels - BeamBreaker.getNavigationBarHeight(context);
+		points = new float[]{0, 0, width, height, width/3, 0, width/3 + width/5, height, width, 0, 0, height};
 	}
 
 	@Override
@@ -34,10 +36,7 @@ public class MyCanvas extends View
 		paint.setTextAlign(Align.CENTER);
 		// canvas.drawText("Yo ho ho!", 100, 100, paint);
 		paint.setStrokeWidth(3);
-		points = new float[]{0, 0, width, height, width/3, 0, width/3 + width/5, height, width, 0, 0, height};
-		///canvas.drawLine(0, 0, width, height, paint);
-		//canvas.drawLine(width / 3, 0, width / 3 + width / 5, height, paint);
-		//canvas.drawLine(width, 0, 0, height, paint);
+		
 		canvas.drawLines(points, paint);
 		super.onDraw(canvas);
 	}
@@ -54,17 +53,37 @@ public class MyCanvas extends View
 			float slope;
 			for (int i = 0; i < points.length; i+=4)
 			{
-				 deltaY = points[i+1] - points[i+3];
-				 deltaX = points[i] - points[i+2];
-				 slope = deltaY/deltaX;
-				 for (int j = 0; j < width; j++)
-				 {
-					 y = slope * (j - points[i]) + points[i+1];
-					 //Point to check is (j, y)
-					 if (Math.abs(event.getX() - j) < 5 && Math.abs(event.getY() - y) < 5)
-						 Log.d("touch", "You touched line " + i/4);
-				 }
+				if (points[i] != -1)
+				{
+					 deltaY = points[i+1] - points[i+3];
+					 deltaX = points[i] - points[i+2];
+					 slope = deltaY/deltaX;
+					 for (int j = 0; j < width; j++)
+					 {
+						 y = slope * (j - points[i]) + points[i+1];
+						 //Point to check is (j, y)
+						 if (Math.abs(event.getX() - j) < 5 && Math.abs(event.getY() - y) < 5)
+						 {
+							 Log.d("touch", "You touched line " + i/4);
+							 linesZapped++;
+							 //Zap it
+							 for (int k = i-4; j < points.length - 4; j++)
+							 {
+								 points[k] = points[k+4];
+							 }
+							 for (int k = 0; k < linesZapped; k++)
+							 {
+								 points[points.length - (4 +(4*k))] = -1;
+								 points[points.length - (3 +(4*k))] = -1;
+								 points[points.length - (2 +(4*k))] = -1;
+								 points[points.length - (1 +(4*k))] = -1;
+							 }
+							 break;
+						 }
+					 }
+				}
 			}
+			invalidate();
 		}
 		return super.onTouchEvent(event);
 	}
